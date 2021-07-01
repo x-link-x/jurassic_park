0f23efd792c7bc9c432eaee22f6a373564b2cb07
Link <xlinkx_@outlook.com>
2021-07-01 14:38:32 +0100

    first test passing

---
+++ models/park.js
@@ -0,0 +1,10 @@
+class Park {
+
+    constructor(name, ticketPrice) {
+        this.name = name;
+        this.ticketPrice = ticketPrice;
+        this.dinosaurs = [];
+    }
+}
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let dinosaur1;
+  let dinosaur2;
+  let dinosaur3;
+  let dinosaur4;
+  let park;
+
+  beforeEach(function () {
+    dinosaur1 = new Dinosaur('t-rex', 'carinvore', 50);
+    dinosaur2 = new Dinosaur('stegosaurus', 'herbivore', 40);
+    dinosaur3 = new Dinosaur('pterodactyl', 'carnivore', 60);
+    dinosaur4 = new Dinosaur('brontosaurus', 'herbivore', 100);
+    park = new Park('Jurassic Park');
+  })
+
+  it('should have a name', function() {
+    actual = park.name;
+    assert.strictEqual(actual, "Jurassic Park");
+  });
+
+  it('should have a ticket price');
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
